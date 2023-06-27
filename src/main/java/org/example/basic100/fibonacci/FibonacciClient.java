6d0fdaf66054dfce6333a4d25bdbde9a84b3b635
idooru <shere1765@gmail.com>
2023-06-27 12:22:44 +0900

    [알고리즘 기초 100제] 2번 피보나치 수열 함수로 구현

--- src/main/java/org/example/basic100/fibonacci/FibonacciClient.java
+++ src/main/java/org/example/basic100/fibonacci/FibonacciClient.java
@@ -8,6 +8,8 @@ public class FibonacciClient {
 
         final int[] ints = fibonacciClient.makeFibonacciArray(12);
         System.out.println(Arrays.toString(ints));
+
+        fibonacciClient.implementFibonacciWithMethod(fibonacciClient);
     }
 
     public int[] makeFibonacciArray(int length) {
@@ -21,5 +23,20 @@ public class FibonacciClient {
         }
 
         return ints;
+    }
+
+    public void implementFibonacciWithMethod(FibonacciClient fibonacciClient) {
+        final int fib = fibonacciClient.fib(10);
+        System.out.println("fib = " + fib);
+    }
+
+    private int fib(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
     }
 }
