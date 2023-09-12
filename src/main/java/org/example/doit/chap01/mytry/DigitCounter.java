2e5d39ee92bdc8493ae890d59f6c84650d735780
Idooru <shere1765@gmail.com>
2023-09-12 18:51:59 +0900

    10진수를 2진수 ~ 36진수로 기수 변환하여 출력하기

--- src/main/java/org/example/doit/chap01/mytry/DigitCounter.java
+++ src/main/java/org/example/doit/chap01/mytry/DigitCounter.java
@@ -9,17 +9,17 @@ public class DigitCounter {
         int n = 0;
 
         do {
-           try {
+            try {
                 System.out.print("양의 정수 입력: ");
                 n = scanner.nextInt();
             } catch (InputMismatchException err) {
                 System.out.println("잘못된 입력입니다. 정수를 입력해주세요.");
-                scanner.nextLine(); 
+                scanner.nextLine();
             }
-        } while(n <= 0);
+        } while (n <= 0);
 
         String stringifyInt = Integer.toString(n);
-        
+
         for (int i = 0; i <= stringifyInt.length(); i++) {
             if (i == stringifyInt.length()) {
                 System.out.printf("그 수는 %d자리입니다.", i);
